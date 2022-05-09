212e2ed65540ab0ab155854eed7be11f15188c66
Anusha Lihala <anusha.lihala@koneksys.com>
2022-05-09 23:08:39 +0530

    Refactor and test _get_neighbours

--- sudoku_solver_refactored.py
+++ sudoku_solver_refactored.py
@@ -5,44 +5,99 @@ from sudoku_solver_exceptions import SudokuSolverException
 class SudokuContext:
     def __init__(self, sudoku_string, sudoku_size=None):
         if sudoku_size is None:
-            self.board_size = len(sudoku_string)
+            self.board_area = len(sudoku_string)
         else:
-            self.board_size = sudoku_size
+            self.board_area = sudoku_size
+
+        self.sudoku_string = sudoku_string
+        self.board_length = int(math.sqrt(self.board_area))
         self.current_board = dict()       # actual values in Sudoku board
         self.current_domain = dict()      # possible value each cell in Sudoku board can take
         self.neighbours = dict()  # CONSTRAINT; all neighbours must be different
 
-        if len(sudoku_string) != self.board_size:
-            raise SudokuSolverException(f"input Sudoku string length not equal to {self.board_size}")
+        if len(sudoku_string) != self.board_area:
+            raise SudokuSolverException(f"input Sudoku string length not equal to {self.board_area}")
+        elif self.board_length**2 != self.board_area:
+            raise SudokuSolverException("input Sudoku string length does not form a square")
 
     def _get_sudoku_indices(self, input_int):
-        # converts linear indices [0-80] to 2d indices [0-8][0-8]
-        divisor = int(math.sqrt(self.board_size))
-        row = input_int // divisor
-        col = input_int % divisor
+        # converts linear indices [0-80] to 2d indices [r][c] with domain [0-8][0-8] of type string
+        row = input_int // self.board_length
+        col = input_int % self.board_length
         return str(row) + str(col)
 
     def _sudoku_string_to_context(self, sudoku_string):
-        for i in range(self.board_size):
-            # storing values from linear string Sudoku board into custom data structure (dictionary with rowcol as index)
-            idx = get_indx(i)  # idx is a two character string with first value=row and second value=column
-            val = int(sb_list[i])
-            self.board[idx] = val
+        for i in range(self.board_area):
+            sudoku_indices = self._get_sudoku_indices(i)
+            cell_value = int(self.sudoku_string[i])
+            self.board[sudoku_indices] = cell_value
 
-            if val == 0:
-                self.domain[idx] = set(range(1, 10))  # initial domain -> if cell empty, can take any value from 1 to 9
+            if cell_value == 0:
+                # initial domain -> if cell empty, can take any value from 1 to 9
+                self.domain[sudoku_indices] = set(range(1, 10))
             else:
-                self.domain[idx] = set([val])  # initial domain -> if cell filled, can take only that value
+                # initial domain -> if cell filled, can take only that value
+                self.domain[sudoku_indices] = set([cell_value])
 
-            self.neighbours[idx] = self.compute_neighbours(idx)
+            self.neighbours[sudoku_indices] = self.compute_neighbours(sudoku_indices)
+
+    def _get_linear_neighbours(self, const_index, const_row_or_col):
+        const_list = [const_index] * self.board_length
+        changing_list = range(0, self.board_length)
+        changing_list = map(lambda x: str(x), changing_list)
+        changing_list = filter(lambda x: x != const_index, changing_list)
+
+        if const_row_or_col == "row":
+            neighbours_list = zip(const_list, changing_list)
+        elif const_row_or_col == "col":
+            neighbours_list = zip(changing_list, const_list)
+        else:
+            raise SudokuSolverException("second argument of _get_linear_neighbours not one of ['row', 'col']")
+
+        neighbours_list = map(lambda x: "".join(x), neighbours_list)
+        neighbours_list = list(neighbours_list)
+        return neighbours_list
+
+    def _get_column_neighbours(self, sudoku_indices):
+        # get neighbours from same column
+        column_index = sudoku_indices[1]
+        return self._get_linear_neighbours(column_index, 'col')
+
+    def _get_row_neighbours(self, sudoku_indices):
+        # get neighbours from same row
+        row_index = sudoku_indices[0]
+        return self._get_linear_neighbours(row_index, 'row')
+
+    def _get_box_neighbours(self, sudoku_indices, box_length=3):
+        row_index = sudoku_indices[0]
+        column_index = sudoku_indices[1]
+
+        number_of_boxes = self.board_length / box_length
+        # check if whole number
+        if number_of_boxes - int(number_of_boxes) != 0:
+            raise SudokuSolverException("self.board_length not divisible by box_length argument value")
+        horizontal_box_number = int(column_index) // number_of_boxes
+        horizontal_box_number = int(horizontal_box_number)
+        vertical_box_number = int(row_index) // number_of_boxes
+        vertical_box_number = int(vertical_box_number)
+
+        neighbours_list = []
+        for row_index in range(box_length * vertical_box_number, box_length * vertical_box_number + box_length):
+            for col_index in range(box_length * horizontal_box_number, box_length * horizontal_box_number + box_length):
+                neighbours_list.append(str(row_index)+str(col_index))
+
+        neighbours_list = list(filter(lambda x: x!=sudoku_indices, neighbours_list))
+        return neighbours_list
+
+    def _get_neighbours(self, sudoku_indices, box_length=3):
+        return self._get_row_neighbours(sudoku_indices) + \
+               self._get_column_neighbours(sudoku_indices) + \
+               self._get_box_neighbours(sudoku_indices, box_length)
+
 
 if __name__=="__main__":
     print("hello main refactored!")
-    # input_test = "010020300004005060070000008006900070000100002030048000500006040000800106008000000"
+    input_test = "010020300004005060070000008006900070000100002030048000500006040000800106008000000"
     # sc = SudokuContext(input_test)
 
-    sudoku_indices = []
-    for i in range(9):
-        for j in range(9):
-            sudoku_indices.append(str(i) + str(j))
-    print(sudoku_indices)
+    sc = SudokuContext("0"*81)
--- sudoku_solver_tests.py
+++ sudoku_solver_tests.py
@@ -17,9 +17,9 @@ class TestSudokuContext(unittest.TestCase):
 
     def test_sudoku_size_validation(self):
         sudoku_context_object = SudokuContext(self.sudoku_boards[0])
-        self.assertEquals(sudoku_context_object.board_size, len(self.sudoku_boards[0]))
+        self.assertEquals(sudoku_context_object.board_area, len(self.sudoku_boards[0]))
         sudoku_context_object_mini = SudokuContext("1234")
-        self.assertEquals(sudoku_context_object_mini.board_size, 4)
+        self.assertEquals(sudoku_context_object_mini.board_area, 4)
 
         self.assertRaises(sudoku_solver_exceptions.SudokuSolverException, SudokuContext, "1234", 5)
 
@@ -34,3 +34,45 @@ class TestSudokuContext(unittest.TestCase):
         for index in range(len(sudoku_indices)):
             self.assertEqual(sudoku_context_object._get_sudoku_indices(index), sudoku_indices[index])
 
+    def test_get_neighbours_4(self):
+        input_sudoku_string = "0"*16
+        sc = SudokuContext(input_sudoku_string)
+
+        # test _get_column_neighbours
+        self.assertEqual(sc._get_column_neighbours("00"), ["10", "20", "30"])
+        self.assertEqual(sc._get_column_neighbours("33"), ["03", "13", "23"])
+
+        # test _get_row_neighbours
+        self.assertEqual(sc._get_row_neighbours("00"), ["01", "02", "03"])
+        self.assertEqual(sc._get_row_neighbours("33"), ["30", "31", "32"])
+
+        # test _get_box_neighbours
+        self.assertEqual(sc._get_box_neighbours("00", 2), ['01', '10', '11'])
+        self.assertEqual(sc._get_box_neighbours("33", 2), ['22', '23', '32'])
+
+        # test _get_neighbours
+        self.assertEqual(sc._get_neighbours("00", 2), ["01", "02", "03"] + ["10", "20", "30"] + ['01', '10', '11'])
+
+    def test_get_neighbours_81(self):
+        input_sudoku_string = "0"*81
+        sc = SudokuContext(input_sudoku_string)
+
+        # test _get_column_neighbours
+        self.assertEqual(sc._get_column_neighbours("00"), ["10", "20", "30", "40", "50", "60", "70", '80'])
+        self.assertEqual(sc._get_column_neighbours("44"), ["04", "14", "24", "34", "54", "64", "74", '84'])
+        self.assertEqual(sc._get_column_neighbours("88"), ["08", "18", "28", "38", "48", "58", "68", "78"])
+
+        # test _get_row_neighbours
+        self.assertEqual(sc._get_row_neighbours("00"), ["01", "02", "03", "04", "05", "06", "07", "08"])
+        self.assertEqual(sc._get_row_neighbours("44"), ["40", "41", "42", "43", "45", "46", "47", "48"])
+        self.assertEqual(sc._get_row_neighbours("88"), ["80", "81", "82", "83", "84", "85", "86", "87"])
+
+        # test _get_box_neighbours
+        self.assertEqual(sc._get_box_neighbours("00"), ['01', '02', '10', '11', '12', '20', '21', '22'])
+        self.assertEqual(sc._get_box_neighbours("44"), ['33', '34', '35', '43', '45', '53', '54', '55'])
+        self.assertEqual(sc._get_box_neighbours("88"), ['66', '67', '68', '76', '77', '78', '86', '87'])
+        self.assertEqual(sc._get_box_neighbours("07"), ['06', '08', '16', '17', '18', '26', '27', '28'])
+        self.assertEqual(sc._get_box_neighbours("70"), ['60', '61', '62', '71', '72', '80', '81', '82'])
+
+        # test _get_neighbours
+        self.assertEqual(sc._get_neighbours("00"), ["01", "02", "03", "04", "05", "06", "07", "08"] + ["10", "20", "30", "40", "50", "60", "70", '80'] + ['01', '02', '10', '11', '12', '20', '21', '22'])
